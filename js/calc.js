1b37fdba721621c609bd4a4e5ceaae5360fdbab8
SOFIAT <abioye.sofiat@outlook.com>
2015-10-19 16:50:57 +0100

    added files successfully

---
+++ js/calc.js
@@ -0,0 +1,60 @@
+ var txt = "";
+var dec= "."
+var deci = "0.";
+
+
+function showVal(num){
+  var txt = document.getElementById("displaybox").value;
+  txt = txt + num;
+  if(displaybox.value.length > 30){
+  	var error = "Maximum length of characters reached!!!";
+  	displaybox.value = error;
+  }else
+  document.getElementById("displaybox").value = txt;
+ 
+}
+
+function clearNum(){
+     txt = " ";
+	document.getElementById("displaybox").value = txt;
+
+}
+
+function calculate(){
+txt = document.getElementById("displaybox").value;
+evaltxt = eval(txt);
+displaybox.value = evaltxt;
+}
+
+function toggle(){
+	txt = document.getElementById("displaybox").value;
+	if(txt == +txt){
+		displaybox.value = -txt;
+	}
+	else if(txt == -txt)
+   displaybox.value = +txt;
+}
+
+function point(){  
+    
+    if(displaybox.value.length == 0)		
+		{
+			displaybox.value = "0";
+		}
+		displaybox.value += dec;
+}
+
+function squareroot(){
+    txt = document.getElementById("displaybox").value;
+    var txt2 = parseInt(txt);
+    var answer = Math.sqrt(txt2);
+    displaybox.value = answer;
+
+}
+
+function inverse(){
+	txt = document.getElementById("displaybox").value;
+	var inverse = eval(1/txt);
+	displaybox.value = inverse;
+
+}
